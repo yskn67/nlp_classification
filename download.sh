aea821a9419ff0ff6f92cd54e5e13df4c9a2e231
yskn67 <yskn1967@gmail.com>
2019-02-12 02:57:46 +0900

    Change dataset

--- download.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-kaggle datasets download mousehead/songlyrics -w --unzip
--- utils.py
+++ utils.py
@@ -2,40 +2,56 @@
 
 import numpy as np
 import tensorflow as tf
+from sklearn.datasets import fetch_20newsgroups
 from sacremoses import MosesTokenizer
 tf.enable_eager_execution()
 
 
-def get_datasets(max_sequence_length=1024, min_frequency=2):
-    train = tf.data.experimental.CsvDataset('train.csv', [tf.string, tf.string], header=True)
-    test = tf.data.experimental.CsvDataset('test.csv', [tf.string, tf.string], header=True)
+target_labels = ['comp', 'rec', 'sci', 'talk']
 
-    artist_participants = train.concatenate(test).map(lambda artist, text: artist).apply(tf.data.experimental.unique())
-    artist_participants = [d.numpy().decode('utf-8') for d in artist_participants]
+
+def get_datasets(max_sequence_length=10000, min_frequency=2):
+    train = fetch_20newsgroups(subset='train', shuffle=True, random_state=57, remove=('header', 'footer', 'quates'))
+    test = fetch_20newsgroups(subset='test', shuffle=True, random_state=57, remove=('header', 'footer', 'quates'))
 
     tokenizer = MosesTokenizer()
+    train_target = []
+    train_text = []
+    for target, text in zip(train.target, train.data):
+        for label in target_labels:
+            if train.target_names[target].startswith(label):
+                target = np.zeros(len(target_labels), dtype=np.float32)
+                target[target_labels.index(label)] = 1.0
+                train_target.append(target)
+                train_text.append([token.lower() for token in tokenizer.tokenize(text)])
+                break
+    train_target = np.array(train_target, dtype=np.float32)
+
+    test_target = []
+    test_text = []
+    for target, text in zip(test.target, test.data):
+        for label in target_labels:
+            if test.target_names[target].startswith(label):
+                target = np.zeros(len(target_labels), dtype=np.float32)
+                target[target_labels.index(label)] = 1.0
+                test_target.append(target)
+                test_text.append([token.lower() for token in tokenizer.tokenize(text)])
+                break
+    test_target = np.array(test_target, dtype=np.float32)
+
     vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
         max_sequence_length, min_frequency=min_frequency)
-    corpus = train.concatenate(test).map(lambda artist, text: text)
-    corpus = [' '.join(tokenizer.tokenize(d.numpy().decode('utf-8'))) for d in corpus]
+    corpus = train_text + test_text
+    corpus = [' '.join(tokens) for tokens in corpus]
     vocab_processor.fit(corpus)
 
-    def make_ohe(artist):
-        ohe = np.zeros(len(artist_participants), dtype=np.float32)
-        ohe[artist_participants.index(artist.decode('utf-8'))] = 1.0
-        return ohe
+    train_text = np.array(list(vocab_processor.transform([' '.join(tokens) for tokens in train_text])), dtype=np.int32)
+    test_text = np.array(list(vocab_processor.transform([' '.join(tokens) for tokens in test_text])), dtype=np.int32)
 
-    def tokenize(text):
-        tokens = tokenizer.tokenize(text.decode('utf-8'))
-        tokens = list(vocab_processor.transform([' '.join(tokens)]))
-        return tokens[0].astype(np.int32)
+    train = tf.data.Dataset.from_tensor_slices({"target": train_target, "text": train_text})
+    test = tf.data.Dataset.from_tensor_slices({"target": test_target, "text": test_text})
 
-    def preprocess(artist, text):
-        ohe = tf.py_func(make_ohe, [artist], [tf.float32])
-        tokens = tf.py_func(tokenize, [text], [tf.int32])
-        return (ohe, tokens)
-
-    return (train.map(preprocess), test.map(preprocess), len(artist_participants), len(vocab_processor.vocabulary_))
+    return (train, test, len(target_labels), len(vocab_processor.vocabulary_))
 
 
 if __name__ == '__main__':
